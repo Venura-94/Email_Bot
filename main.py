779fed83974510cf53b994ed40194cd56e2222bf
Venura Pussella <venurapussella123@gmail.com>
2024-01-21 19:59:47 +0530

    changes made 1 and 2

--- main.py
+++ main.py
@@ -3,18 +3,15 @@ import pandas as pd
 from send_email import send_email #local python module
 
 #public googlesheet url - not secure
-#SHEET_ID = "<1OmQPYrEuLT38EtxNHHor7fjnTDo1cWz5lgyyPcIlho4>"
-#SHEET_NAME = "<Sheet1>"
-#URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}"
-
-URL = f"https://docs.google.com/spreadsheets/d/1OmQPYrEuLT38EtxNHHor7fjnTDo1cWz5lgyyPcIlho4/edit#gid=0"
+SHEET_ID = "1OmQPYrEuLT38EtxNHHor7fjnTDo1cWz5lgyyPcIlho4"
+SHEET_NAME = "Sheet1"
+URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}" #sametime this converts google data sheet pandas data frame
 
 def load_df(url):
-    parse_dates = ["due date","reminder date"]
+    parse_dates = ["due_date","reminder_date"]
+
     df =pd.read_csv(url, parse_dates=parse_dates)
     return df
-
-print(load_df(URL))
 
 def query_data_and_send_emails(df):
     present = date.today()
@@ -22,16 +19,17 @@ def query_data_and_send_emails(df):
     for _, row in df.iterrows():
         if (present >= row["reminder_date"].date()) and (row["has_paid"] == "no"):
             send_email(
-                subject=f'[Dozen Invoice: {row["invoice_no"]}]',
+                subject=f'[Dozen Pvt Ltd] Invoice: {row["invoice_no"]}',
                 receiver_email=row["email"],
                 name=row["name"],
-                due_date=row["due_date"].strftime("%d,%b %Y"),  # Date example
+                due_date=row["due_date"].strftime("%d, %b %Y"), 
                 invoice_no=row["invoice_no"],
                 amount=row["amount"],
             )
             email_counter += 1
-    return f"total Emails Sent: {email_counter}"
+    return f"Total Emails Sent: {email_counter}"
 
-df = load_df(URL)
-result = query_data_and_send_emails(df)
+df = load_df(URL) #loading dataframe
+result = query_data_and_send_emails(df) #seeing the results as counts
 print(result)
+
